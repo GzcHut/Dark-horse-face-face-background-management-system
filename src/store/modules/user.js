c6e692e8858d85517323e235bb1e04de868aa7a2
GzcHut <gzchut@qq.com>
2023-03-10 09:23:17 +0800

    黑马面面初始化

---
+++ src/store/modules/user.js
@@ -0,0 +1,17 @@
+const getDefaultState = () => {
+  return {}
+}
+
+const state = getDefaultState()
+
+const mutations = {}
+
+const actions = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
